14ace76962ca0d6032984fcfc12859c2af8aea8d
Alisa427 <abrujic1@etf.unsa.ba>
2020-10-26 16:07:30 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/tut1/zad3/Sat.java
@@ -0,0 +1,78 @@
+package ba.unsa.etf.rpr.tut1.zad3;
+
+public class Sat {
+    int sati, minute, sekunde;
+
+    Sat(int sati, int minute, int sekunde) {
+        Postavi(sati, minute, sekunde);
+    }
+
+    void Postavi(int sati, int minute, int sekunde) {
+        this.sati = sati;
+        this.minute = minute;
+        this.sekunde = sekunde;
+    }
+
+    void Sljedeci() {
+        sekunde++;
+        if (sekunde == 60) {
+            sekunde = 0;
+            minute++;
+        }
+        if (minute == 60) {
+            minute = 0;
+            sati++;
+        }
+        if (sati == 24) sati = 0;
+    }
+
+    void Prethodni() {
+        sekunde--;
+        if (sekunde == -1) {
+            sekunde = 59;
+            minute--;
+        }
+        if (minute == -1) {
+            minute = 59;
+            sati--;
+        }
+        if (sati == -1) sati = 23;
+    }
+
+    void PomjeriZa(int pomak) {
+        int i = 0;
+        if (pomak > 0)
+            while (i < pomak) {
+                Sljedeci();
+                i = i + 1;
+            }
+        else while (i< -pomak) {
+            Prethodni();
+            i = i + 1;
+        }
+        i = 0;
+    }
+
+    int DajSate()
+
+    {
+        return sati;
+    }
+
+    int DajMinute()
+
+    {
+        return minute;
+    }
+
+    int DajSekunde()
+
+    {
+        return sekunde;
+    }
+
+    void Ispisi()
+    {
+        System.out.println(sati+":"+minute+":"+sekunde);
+    }
+}
